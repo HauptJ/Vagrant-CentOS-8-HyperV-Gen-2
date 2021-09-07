446fcd10ff95296f3d8c60919a5f1f4cc4314771
Joshua Haupt <joshua@hauptj.com>
2021-09-07 10:09:29 -0500

    Build image for Centos 8.4

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 sudo wget -O /etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
-sudo dnf install -y https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-8.el8.noarch.rpm
+sudo dnf install -y http://mirror.centos.org/centos/8/extras/aarch64/os/Packages/epel-release-8-11.el8.noarch.rpm
 sudo dnf update -y
 sudo dnf install -y ansible
